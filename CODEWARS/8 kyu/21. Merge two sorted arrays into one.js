36360266be4831d4fb6a505dcd49e16eeca28815
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-06-30 10:27:32 +0300

    [CODEWARS] 22. Exclusive "or" (xor) Logical Operator

--- CODEWARS/8 kyu/21. Merge two sorted arrays into one.js
+++ CODEWARS/8 kyu/21. Merge two sorted arrays into one.js
@@ -1,3 +1,3 @@
 function mergeArrays(arr1, arr2) {
-    return [...new Set([...arr1, ...arr2].sort((a, b) => a - b))]
-  }
+  return [...new Set([...arr1, ...arr2].sort((a, b) => a - b))];
+}
---
+++ CODEWARS/8 kyu/22. Exclusive "or" (xor) Logical Operator.js
@@ -0,0 +1,10 @@
+function xor(a, b) {
+    // TODO: Program Me
+    return a
+            ? b
+              ? false
+              : true
+            : b
+              ? true
+              : false
+  }
